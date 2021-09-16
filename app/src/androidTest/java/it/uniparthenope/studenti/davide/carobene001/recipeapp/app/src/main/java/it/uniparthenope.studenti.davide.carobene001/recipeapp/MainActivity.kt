5ed3b016c9e1d253cb049d08810470fc3cf71620
Bernardo Iamicella <bernardo.iamicella001@uniparthenope.it>
2021-09-16 19:26:55 +0200

    Initial commit

---
+++ app/src/androidTest/java/it/uniparthenope/studenti/davide/carobene001/recipeapp/app/src/main/java/it/uniparthenope.studenti.davide.carobene001/recipeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.uniparthenope.studenti.davide.carobene001.recipeapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
